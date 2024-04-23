fc2cb06dd7a11982db442d0070e6007292b8f91c
shiyubei <shiyubei@qince.com>
2024-04-23 20:24:56 +0800

    feat: 引入原example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Origin from "./Origin";
 import Reform from "./Reform";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
+    <Origin />
     <Reform />
   </>
 );
